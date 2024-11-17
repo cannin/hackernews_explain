415f56cbf34644bdea17eecb69a0ee2561d95009
cannin <790284+cannin@users.noreply.github.com>
2024-11-17 14:42:36 -0500

    Remove redundant instructions for HTML bolding in summary generation prompt in script.js

--- script.js
+++ script.js
@@ -63,9 +63,9 @@ window.onload = function() {
                 description = tmp.textContent || tmp.innerText || "";
 
                 // Create the prompt for the API
-                const prompt = `Read the following title and description to generate a summary of the title and comment in the description tag that includes very brief explanations for high school students of all main technology topics mentioned (e.g., software, tools, APIs, companies, acronyms, tech jargon). The text summary must not have XML tags, no new lines, no paraphrasing or repeating the title, and should be in English. Do not repeat that you are making a summary.
+                const prompt = `Read the following title and description to generate a summary of the title and comment in the description tag that includes short explanations for high school students of all main technology topics mentioned (e.g., software, tools, APIs, companies, acronyms, tech jargon). The text summary must not have XML tags, no new lines, no paraphrasing or repeating the title, and should be in English. Do not repeat that you are making a summary.
 
-                Examples of explanations: 
+                Examples of good short explanations: 
                 * A/B tests (comparing two versions of something to see which works better) 
                 * dogfooding (testing products internally before public release)
                 * PyPI (Python Package Index, a repository of software packages for Python)
@@ -74,9 +74,7 @@ window.onload = function() {
                 * Google (a multinational technology company that specializes in Internet-related services and products)
                 * IntelliJ IDEA (a Java integrated development environment for software developers)
                 
-                Also, put bold <b> HTML tags around important words and keywords in the summaries. Highlight key words and phrases (e.g., names, institutes, locations, amounts) with bold. At least 1 word must be made bold per summary. IMPORTANT USE HTML <b> not Markdown tags!!! Example: <b>Biden</b> visited <b>Vietnam</b> today. 
-                
-                You must translate your response to this language: ${language}
+                You must respond in this language: ${language}
                 
                 \n\nTitle: ${title}\nDescription: ${description}`;
               
